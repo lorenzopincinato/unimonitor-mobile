287df84cbeb3bcaf964be087e5e06b53c0bddf27
Lorenzo Pincinato <lorenzopincinato@gmail.com>
2021-04-06 20:42:40 -0300

    example

--- App.tsx
+++ App.tsx
@@ -1,11 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
+  const [numero, setMeuNumero] = useState(0);
+  const [ehPar, setEhPar] = useState(false);
+
+  const incrementarNumero = () => {
+    setMeuNumero(numero + 1)
+  }
+
+  useEffect(() => {
+    setEhPar(numero % 2 === 0);
+  }, [numero])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>{numero}</Text>
+      <Button title="BOTÃO" onPress={incrementarNumero} />
+      {ehPar ? <Text>Numero é par</Text> : <Text>Número é ímpar</Text>}
       <StatusBar style="auto" />
     </View>
   );
